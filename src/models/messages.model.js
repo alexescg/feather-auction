fa2c5fff2edabe1363154705e5557914d234aa8d
alex <aeg0204@gmail.com>
2017-05-25 22:31:53 -0600

    Implement basic chat app

--- src/models/messages.model.js
+++ src/models/messages.model.js
@@ -1,10 +1,11 @@
 // messages-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const messages = new mongooseClient.Schema({
+    name: { type: String, required: true },
     text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
